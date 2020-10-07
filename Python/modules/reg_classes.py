2bcad9722347fafc0dcb2eaaadfdfebaf5528360
afpsaros <70901418+afpsaros@users.noreply.github.com>
2020-10-07 09:34:54 -0400

    add callbacks

--- Python/modules/reg_classes.py
+++ Python/modules/reg_classes.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 import numpy as np
 import time
 
+from callbacks import *
+
 class scores:
     def errors(self):     
         error = tf.reduce_mean(tf.square(self.fp - self.Y))
@@ -43,29 +45,28 @@ class regressor(scores):
         
     def close_sess(self):
         self.sess.close()
-        
-    
+            
     def fit_from_dict(self, fit_dict):
         return self.fit(*list(fit_dict.values()))  
+      
+    def fit(self, callbacks, initialize, wd_par, num_epochs, Xt, Yt, \
+            Xv = None, Yv = None, lr = None, batches = None):
 
-    def adv_fit_from_dict(self, fit_dict):
-        return self.adv_fit(*list(fit_dict.values()))     
-    
-    def fit(self, initialize, wd_par, num_epochs, Xt, Yt, \
-            snap_step = None, Xv = None, Yv = None, lr = None, batches = None):
-# =============================================================================
-#     def fit(self, wd_par, num_epochs, Xt, Yt, \
-#             Xv = None, Yv = None, lr = None, batches = None):
-# =============================================================================
         self.initialize = initialize
         self.wd_par = wd_par
         self.num_epochs = num_epochs
         if self.initialize == 1:
             self.Xt = Xt
             self.Yt = Yt    
-            
+        
+        self.Xv = Xv
+        self.Yv = Yv
         self.snap_step = snap_step
         self.lr = lr
+        
+##############################################        
+        self.callbacks = CallbackList(callbacks = callbacks)
+        self.callbacks.set_model(self)
 # =============================================================================
 #         self.batches = batches
 #         
@@ -108,84 +109,18 @@ class regressor(scores):
 #             for epoch in range(self.num_epochs): 
 #                 self.sess.run(self.train_op, feed_dict={self.X: self.Xt, self.Y: self.Yt}) 
 # =============================================================================        
-        if self.snap_step is not None:
-            self.snap_weights = []
-            self.snap_biases = []
-            for epoch in range(self.num_epochs): 
-                self.sess.run(self.train_op, feed_dict={self.X: self.Xt, self.Y: self.Yt}) 
+        self.callbacks.on_train_begin()
+        
+        for self.epoch in range(self.num_epochs): 
+            self.callbacks.on_epoch_begin()
             
-                if (epoch + 1) % self.snap_step == 0 and epoch != self.num_epochs - 1:           
-                    self.snap_weights.append(self.sess.run(self.weights))
-                    self.snap_biases.append(self.sess.run(self.biases)) # after update
-                
-        else: 
-            for epoch in range(self.num_epochs): 
-                self.sess.run(self.train_op, feed_dict={self.X: self.Xt, self.Y: self.Yt}) 
-
-        return self
-    
-    def adv_fit(self, initialize, wd_par, num_epochs, Xt, Yt, Xv, Yv, lr = None):
-        
-        self.initialize = initialize
-        self.wd_par = wd_par   
-        self.num_epochs = num_epochs
-        if self.initialize == 1:
-            self.Xt = Xt
-            self.Yt = Yt    
-        self.Xv = Xv
-        self.Yv = Yv
-        self.lr = lr
-        
-        if self.initialize == 1:
-            self.hyper_initial()
-            self.fp = self.ffn()  
-            self.error_mean = self.errors()[0]
-        
-        self.obj = self.error_mean + \
-                        self.wd_par * tf.reduce_sum([tf.reduce_sum(tf.square(self.weights[i])) for i in self.weights])
-                
-        if self.lr is not None:
-            self.optimizer = tf.train.AdamOptimizer(learning_rate = self.lr)
-        
-        self.train_op = self.optimizer.minimize(self.obj)
-        
-        if self.initialize == 1:
-            self.init = tf.global_variables_initializer()
-            self.sess.run(tf.global_variables_initializer())
-        else: 
-            self.sess.run([self.init, tf.variables_initializer(self.optimizer.variables())])   
-        
-        self.check_w, self.check_b, self.tr_error, self.val_error = \
-                    [[] for i in range(4)]   # not 4 * [[]] 
-        
-        for epoch in range(self.num_epochs):
-            self.val_error.append(self.sess.run(self.error_mean, \
-                  feed_dict={self.X: self.Xv, self.Y: self.Yv}))
-            check_w_, check_b_, tr_error_, _ = \
-            self.sess.run([self.weights, self.biases, self.error_mean, self.train_op], \
-                                                           feed_dict={self.X: self.Xt, self.Y: self.Yt})
+            self.sess.run(self.train_op, feed_dict={self.X: self.Xt, self.Y: self.Yt}) 
+                      
+            self.callbacks.at_step()
             
-            self.check_w.append(check_w_)
-            self.check_b.append(check_b_)
-            self.tr_error.append(tr_error_)
-           
-        self.val_error.append(self.sess.run(self.error_mean, \
-              feed_dict={self.X: self.Xv, self.Y: self.Yv}))
-        check_w_, check_b_, tr_error_ = \
-            self.sess.run([self.weights, self.biases, self.error_mean], \
-                                                       feed_dict={self.X: self.Xt, self.Y: self.Yt})        
-        self.check_w.append(check_w_)
-        self.check_b.append(check_b_)
-        self.tr_error.append(tr_error_)
-               
-        return self
-    
-# =============================================================================
-#     def next_batch(self, b):
-#         Xb = self.Xt[b * self.batch_size : (b + 1) * self.batch_size - 1]
-#         Yb = self.Yt[b * self.batch_size : (b + 1) * self.batch_size - 1]
-#         return Xb, Yb
-# =============================================================================
+            self.callbacks.on_epoch_end()
+                                
+        self.callbacks.on_train_end()       
     
 class DNN(regressor):
     def __init__(self, widths, sess, seed):
@@ -269,8 +204,19 @@ if __name__ == '__main__':
         'number of layers': 2,
         'layer width': 50, 
     }
+
+    callbacks = []
     
+    snap_step = 100
+    snap = None if snap_step is None else Snapper(snap_step)   
+    if snap is not None: callbacks.append(snap) 
+    
+    loss_hist = 1
+    loss = None if loss_hist == 0 else Losshistory()   
+    if loss is not None: callbacks.append(loss) 
+
     fit_dict = {
+        'callbacks': callbacks,
         'initialize': 0,
         'wd_par': 0,
         'num_epochs': 1000,
@@ -290,20 +236,17 @@ if __name__ == '__main__':
     model = DNN.standard(DNN_dict, sess, seed = 1)
     model.initialize(fit_dict['Xt'], fit_dict['Yt'])
     
-    advanced_fit = 0
-    
-    if advanced_fit == 1:
-        model.adv_fit_from_dict(fit_dict)
-    
-        plt.yscale('log')
-        plt.plot(model.tr_error, label = 'training loss')
-        plt.plot(model.val_error, label = 'validation error')
-        plt.legend()
-        plt.show()
-    else:
-        model.fit_from_dict(fit_dict)
+    model.fit_from_dict(fit_dict)
         
+#%%
+    snap_weights, snap_biases = snap.get_snaps()
+    tr_error, val_error = loss.get_loss_history()
     
+    plt.yscale('log')
+    plt.plot(tr_error, label = 'training loss')
+    plt.plot(val_error, label = 'validation error')
+    plt.legend()
+    plt.show()
 #%%           
     if scale == 1:
         x = data.Xe.reshape(-1,1)
@@ -327,8 +270,8 @@ if __name__ == '__main__':
         plt.plot(x, data.assess_pred(pred)[1])
         
 #%%
-    for i in [10, 500, 900]:
-        pred = model.pred_w(x_scal, check_w[i], check_b[i])
+    for i in range(len(snap_weights)):
+        pred = model.pred_w(x_scal, snap_weights[i], snap_biases[i])
         pred = data.scaler_y.inverse_transform(pred)
         plt.plot(x, pred, '.', label = '{}'.format(i))
         
@@ -336,22 +279,22 @@ if __name__ == '__main__':
     plt.legend()    
     plt.show()
 #%%    
-    tocs = []
+    # tocs = []
     
-    tic1 = time.perf_counter()
-    for i in range(fit_dict['num_epochs']):
-        tic = time.perf_counter()
+    # tic1 = time.perf_counter()
+    # for i in range(fit_dict['num_epochs']):
+    #     tic = time.perf_counter()
         
-        g = tf.Graph()
-        sess = tf.Session(graph = g)
-        with g.as_default() as g:
-            model = DNN.standard(DNN_dict, sess, seed = 1)        
-            pred = model.pred_w(x_scal, check_w[i], check_b[i])
-        tocs.append(time.perf_counter() - tic)
+    #     g = tf.Graph()
+    #     sess = tf.Session(graph = g)
+    #     with g.as_default() as g:
+    #         model = DNN.standard(DNN_dict, sess, seed = 1)        
+    #         pred = model.pred_w(x_scal, check_w[i], check_b[i])
+    #     tocs.append(time.perf_counter() - tic)
      
-    print(time.perf_counter() - tic1)    
-    plt.plot(tocs)
-    plt.show()
+    # print(time.perf_counter() - tic1)    
+    # plt.plot(tocs)
+    # plt.show()
 
     
 
